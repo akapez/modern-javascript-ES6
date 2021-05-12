ce78c849c0fb62862cb632245ea43321d766313e
avishka964 <avishkakapuruge@gmail.com>
2021-05-12 08:23:56 +0530

    update lecture_13

---
+++ chapter_13/sandbox_1.js
@@ -0,0 +1,69 @@
+const list = document.querySelector('ul');
+const form = document.querySelector('form');
+
+const addRecipe = (recipe, id) => {
+
+    let time = recipe.created_at.toDate();
+    let html = `
+         <li data-id="${id}">
+         <div>${recipe.title}</div>
+         <div>${time}</div>
+         <button class='btn btn-danger btn-sm my-2'>delete</button>
+         </li>
+    `
+    list.innerHTML += html;
+}
+
+const deleteRecipe = (id) => {
+    const recipes = document.querySelectorAll('li');
+    recipes.forEach(recipe => {
+        if(recipe.getAttribute('data-id') === id){
+            recipe.remove();
+        }
+    })
+}
+
+//get data
+db.collection('recipes').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+        const doc = change.doc;
+        if(change.type === 'added'){
+            addRecipe(doc.data(), doc.id);
+        }else if(change.type === 'removed'){
+            deleteRecipe(doc.id);
+        }
+    })
+})
+
+
+//add data
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const now = new Date();
+    const recipe = {
+        title: form.recipe.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+
+    db.collection('recipes').add(recipe)
+    .then(() => {
+        console.log('recipe added');
+    }).catch(err => {
+        console.log(err);
+    });
+});
+
+//delete data
+list.addEventListener('click', e => {
+
+    if(e.target.tagName === 'BUTTON'){
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collection('recipes').doc(id).delete()
+        .then(() => {
+            console.log('recipe deleted');
+        }).catch(err => {
+            console.log(err);
+        });
+    };
+});
